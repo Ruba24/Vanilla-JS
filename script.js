be90544b82c033bde92aa1725f195820f4d46111
ruba24 <rubaqazi2000@gmail.com>
2024-09-05 10:19:33 +0500

    Learning JS

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-var close = document.getElementById("cross");
-var i;
-for(i = 0; i < close.length; i++){
-    close[i].onClick = function() {
-        var div = this.parentElement;
-        div.style.display = "none";
-    }
-}
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
-console.log("Js is working")
+var name = "Ruba";
+var age = "24";
+console.log(`My Name is ${name} and my age is ${age}.`);
+var months = age *12; 
+console.log("My age is", age, "Years");
+console.log("My age in months is ", months);
+var days = age * 365;
+console.log("My age in days is", days);
+var hours = days * 24;
+console.log("My age in hours is", hours);
+let a;
+console.log(a);
+let b = 20;
+let c = 10;
+console.log("Value of b is", b);
+console.log("Value of c is", c);
+console.log("There sum is ", b+c);
+console.log("There multiplication is", b*c);
+console.log("There division is", b/c);
+console.log("There difference is", b-c);
+
+  

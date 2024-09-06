eba2ae98bb639c281fff437b285a32d69a28bb22
ruba24 <rubaqazi2000@gmail.com>
2024-09-06 10:57:09 +0500

    Data Types Practice

--- script.js
+++ script.js
@@ -1,22 +1,85 @@
-var name = "Ruba";
-var age = "24";
-console.log(`My Name is ${name} and my age is ${age}.`);
-var months = age *12; 
-console.log("My age is", age, "Years");
-console.log("My age in months is ", months);
-var days = age * 365;
-console.log("My age in days is", days);
-var hours = days * 24;
-console.log("My age in hours is", hours);
-let a;
-console.log(a);
-let b = 20;
-let c = 10;
-console.log("Value of b is", b);
-console.log("Value of c is", c);
-console.log("There sum is ", b+c);
-console.log("There multiplication is", b*c);
-console.log("There division is", b/c);
-console.log("There difference is", b-c);
+// let name = "Ruba";
+// var age = "24";
+// console.log(`My Name is ${name} and my age is ${age}.`);
+// var months = age *12; 
+// console.log("My age is", age, "Years");
+// console.log("My age in months is ", months);
+// var days = age * 365;
+// console.log("My age in days is", days);
+// var hours = days * 24;
+// console.log("My age in hours is", hours);
+// let a;
+// console.log(a);
+// let b = 20;
+// let c = 10;
+// console.log("Value of b is", b);
+// console.log("Value of c is", c);
+// console.log("There sum is ", b+c);
+// console.log("There multiplication is", b*c);
+// console.log("There division is", b/c);
+// console.log("There difference is", b-c);
 
-  
+//   let myName, salary;
+//   myName="Ruba";
+//   salary=25000;
+//   console.log(age,myName,salary);
+
+
+// let mySalary = 20000;
+// let myName = "Ruba";
+// let status = true;
+// let age;
+// let attendence = null;
+// let days = BigInt(34378456499324);
+// console.log("Name:", myName);
+// console.log(typeof myName);
+
+// console.log("Salary:", mySalary);
+// console.log(typeof mySalary);
+
+// console.log("Age:", age);
+// console.log(typeof age);
+
+// console.log("Attendence:", attendence);
+// console.log(typeof attendence);
+
+// console.log("Employed:", status);
+// console.log(typeof status);
+
+// console.log(typeof days);
+
+
+// let car = {
+//     name:"Audi",
+//     color: "Grey",
+//     tranmission: "Manual",
+//     Model: 2010,
+
+// };
+// console.log(car);
+// console.log(car.name);
+// console.log(car["Model"], car["tranmission"]);
+
+
+let keyboard = {
+    price: "$27.75",
+    color: "k1 white",
+    storeName: "MageGee Store",
+    purchase: "1k+ bought in Past Month",
+    features: "RGB LED, 104 key",
+    compatibility: "PC/Laptop",
+    items: "Keyboard & Mouse",
+    ratings: 14132,
+    returnPolicy: true,
+    replacementPolicy:true,
+};
+console.log(keyboard);
+
+let car = {
+    Name: "Dodge",
+    model: "2020",
+    series: "Challenger",
+    transmission: "Manual",
+    gearbox: "6 gears"
+};
+console.log(car);
